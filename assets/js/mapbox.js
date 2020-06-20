8daee7c90461eebecb767b201db1c4b343e1647f
Lucien Liz-Lepiorz <lucien.lizlepiorz@gmail.com>
2020-06-19 22:41:59 -0500

    added dummy overlay

--- assets/js/mapbox.js
+++ assets/js/mapbox.js
@@ -1,8 +1,10 @@
 mapboxgl.accessToken = "pk.eyJ1IjoibHVjaWVubGl6bGVwaW9yeiIsImEiOiJjaWluY3lweWUwMWU5dHBrcHlsYnpscjF5In0.siT3_mzRABrCBeG4iGCEYQ";
+
 var bounds = [
 	[-88.045342, 41.612104], // Southwest coordinates
 	[-87.326560, 42.181465] // Northeast coordinates
 ];
+
 var map = new mapboxgl.Map({
 	container: "map",
 	style: "mapbox://styles/mapbox/dark-v10",
@@ -36,5 +38,5 @@ map.on("load", function() {
 			"circle-stroke-color": "#660000",
 			"circle-stroke-width": 1,
 		}
-	});
+	});		
 });

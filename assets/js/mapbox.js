fed7b4abcea3a2a95b87406d7ce32522ea10bc69
Lucien Liz-Lepiorz <lucien.lizlepiorz@gmail.com>
2020-06-20 11:58:43 -0500

    added logic to focus on relevant data points

--- assets/js/mapbox.js
+++ assets/js/mapbox.js
@@ -13,30 +13,68 @@ var map = new mapboxgl.Map({
 	maxBounds: bounds
 });
 
+var url = "assets/data/features.geojson";
+var layerIDs = [];
+var searchInput = document.getElementById("search-input");
+
 map.addControl(new mapboxgl.NavigationControl());
 
 map.on("load", function() {
-	map.addSource("points", {
-		type: "geojson",
-		data: "assets/data/features.geojson"
-	});			
-	map.addLayer({
-		"id": "points",
-		"type": "circle",
-		"source": "points",
-		"paint": {
-			"circle-radius": 4,
-			"circle-color": [
-				"step",
-				["get", "Owned"],
-				"#000",
-				0, "#ff9900",
-				5, "#990000",
-				50, "#ff6666",
-				200, "#ff9999"
-			],
-			"circle-stroke-color": "#660000",
-			"circle-stroke-width": 1,
-		}
+	// Load GeoJSON
+	var request = new XMLHttpRequest();
+    request.open("GET", url, true);
+    request.onload = function() {
+        if (this.status >= 200 && this.status < 400) {
+            var json = JSON.parse(this.response);
+            console.log(json);
+
+            map.addSource("buildings", {
+				type: "geojson",
+				data: json
+			});
+
+            json.features.forEach(function(feature) {	
+				var address = feature.properties['Address'];
+				var layerID = address.trim().toLowerCase();
+
+				if (!map.getLayer(layerID)) {
+					map.addLayer({
+						"id": layerID,
+						"type": "circle",
+						"source": "buildings",
+						"paint": {
+							"circle-radius": 4,
+							"circle-color": [
+								"step",
+								["get", "Owned"],
+								"#000",
+								0, "#ff9900",
+								5, "#990000",
+								50, "#ff6666",
+								200, "#ff9999"
+							],
+							"circle-stroke-color": "#660000",
+							"circle-stroke-width": 1,
+						},
+						"filter": ["==", "Address", address]
+					});
+					layerIDs.push(layerID);
+					console.log(layerID);
+				}
+			});
+        }
+    };
+    request.send();
+
+	searchInput.addEventListener("keyup", function(e) {
+		var value = e.target.value.trim().toLowerCase();
+		console.log(value);
+		layerIDs.forEach(function(layerID) {
+			map.setLayoutProperty(
+				layerID,
+				"visibility",
+				layerID.indexOf(value) > -1 ? "visible" : "none"
+			);
+		});
 	});		
 });

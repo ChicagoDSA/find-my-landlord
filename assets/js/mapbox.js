fe6712bd964ac5cf3d2f8cbab4b6ed816c93de24
Lucien Liz-Lepiorz <lucien.lizlepiorz@gmail.com>
2020-06-23 12:06:50 -0500

    search now returns simple list, on click loads details, zooms to point, cleaned css

--- assets/js/mapbox.js
+++ assets/js/mapbox.js
@@ -4,7 +4,7 @@ var map = new mapboxgl.Map({
 	container: "map",
 	style: "mapbox://styles/mapbox/dark-v10",
 	center: [-87.695787, 41.881302], // Fred Hampton mural
-	zoom: 11,
+	zoom: 10,
 });
 
 var url = "assets/data/features.geojson";
@@ -37,9 +37,21 @@ function renderResults(features) {
 		});
 	} else if (features.length == 0 && searchInput.value != "") {
 		// No results found
+		// Show container
 		searchResultsContainer.style.display = "block";
-		searchResultsCounter.innerHTML = "<h4>No search results</h4>";
-		searchResultsList.innerHTML = "<p style='margin: 0'>Sorry, we couldn't find that address. Try something like <b>634 E 50th Pl</b>.</p>";
+
+		// Create elements
+		const headline = document.createElement("h4");
+		const description = document.createElement("p");
+
+		// Set values
+		headline.innerHTML = "No search results";
+		description.className = "empty-container";
+		description.innerHTML = "Sorry, we couldn't find that address. Try something like <b>634 E 50th Pl</b>.";
+
+		// Add content to containers
+		searchResultsCounter.appendChild(headline);
+		searchResultsList.appendChild(description);
 	};
 
 	if (searchInput.value == "") {
@@ -49,14 +61,88 @@ function renderResults(features) {
 };
 
 function createListItem(feature) {
-	var item = document.createElement("div");
+	const item = document.createElement("div");
+	const addressText = document.createElement("p");
 	var address = feature.properties["Property Address"];
-	var owner = feature.properties["Owner Name"];
-	var owned = feature.properties["Properties Held by Owner"];
 
 	item.className = "search-result";
-	item.innerHTML = "<h3>"+address+"</h3><p>Owned by: "+owner+"</br>Total properties owned: "+owned+"</p><button type='button'>Download their data</button>";
+	addressText.innerHTML = address;
+
+	// Highlight part of string that matches input 
+	highlightText(searchInput, addressText);
+
+	item.appendChild(addressText);
 	searchResultsList.appendChild(item);
+
+	// Add click event
+	item.onclick = function(){
+		highlightPoint(feature, address);
+	};
+};
+
+function highlightText(input, destination) {
+	var regex = new RegExp(input.value, 'gi')
+	var response = destination.innerHTML.replace(regex, function(str) {
+		return "<b>" + str + "</b>"
+	});
+	destination.innerHTML = response;
+};
+
+function highlightPoint(feature, address) {
+	searchInput.value = address;
+
+	// Center map on address
+	map.flyTo({
+		center: feature.geometry.coordinates,
+		zoom: 12,
+		essential: true
+	});
+
+	for (var i = 0; i < buildings.length; i++) {
+		var objAtIndex = buildings[i].properties["Property Address"]; 
+		if (address === objAtIndex) {
+			// Show address at full opacity
+			map.setPaintProperty(objAtIndex, 'circle-opacity', 1);
+		}
+		else {
+			// Dim other points
+			map.setPaintProperty(objAtIndex, 'circle-opacity', .2);
+		};
+	};
+
+	// Clear counter and list HTML
+	searchResultsCounter.innerHTML = "";
+	searchResultsList.innerHTML = "";
+
+	// Create elements
+	const headline = document.createElement("h4");
+	const container = document.createElement("div");
+	const addressText = document.createElement("h3");
+	const ownerText = document.createElement("p");
+	const ownedText = document.createElement("p");
+
+	// Set values
+	var owner = feature.properties["Owner Name"];
+	var owned = feature.properties["Properties Held by Owner"];
+	headline.innerHTML = "Details";
+	container.className = "empty-container";
+	addressText.innerHTML = address;
+	ownerText.innerHTML = "Owner: "+owner;
+	ownedText.innerHTML = "Total properties owned: "+owned;
+
+	// Add content to containers
+	searchResultsCounter.appendChild(headline);
+	searchResultsList.appendChild(container);
+	container.appendChild(addressText);
+	container.appendChild(ownerText);
+	container.appendChild(ownedText);
+};
+
+function clearPointStyles() {
+	for (var i = 0; i < buildings.length; i++) {
+		var objAtIndex = buildings[i].properties["Property Address"]; 
+		map.setPaintProperty(objAtIndex, 'circle-opacity', 1);
+	};
 };
 
 function clearSearchResults() {
@@ -66,16 +152,6 @@ function clearSearchResults() {
 	// Clear counter and list HTML
 	searchResultsCounter.innerHTML = "";
 	searchResultsList.innerHTML = "";
-};
-
-function containsObject(obj, list) {
-    var i;
-    for (i = 0; i < list.length; i++) {
-        if (list[i] === obj) {
-            return true;
-        };
-    };
-    return false;
 };
 
 map.on("load", function() {
@@ -135,47 +211,18 @@ map.on("load", function() {
 function matchAddresses(e) {
 	var value = e.target.value.trim().toLowerCase();
 	console.log("key up ["+value+"]");
-	
+
+	// Reset highlighted points
+	clearPointStyles();
+
 	// Create list of search results
 	var results = buildings.filter(function(feature) {
 		var address = feature.properties["Property Address"].trim().toLowerCase();
 		// Return feature when trimmed input is found in buildings array
 		return address.indexOf(value) > -1;
 	});
+
+	// Render list
 	console.log("search results populated");
-
-	buildings.forEach(function(e) {	
-		var layerID = e.properties["Property Address"];
-
-		// If building is within search results, show it 
-		if (containsObject(e, results)) {
-			map.setLayoutProperty(
-				layerID,
-				"visibility",
-				"visible"
-			);
-		} else {
-			// Hide it otherwise
-			map.setLayoutProperty(
-				layerID,
-				"visibility",
-				"none"
-			);
-		};	
-	});
-	// Call function once map is rendered
-	map.on("render", afterChangeComplete);
-
-	function afterChangeComplete () {
-		// Map isn't loaded, bail out
-		if (!map.loaded()) { 
-			return; 
-		};
-
-		// Map is loaded, render list
-		renderResults(results);
-
-		// Remove handler once completed
-		map.off("render", afterChangeComplete);
-	};
+	renderResults(results);
 };

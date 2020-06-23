9be9783cff072dbe6f17340ccca039f321665a75
Lucien Liz-Lepiorz <lucien.lizlepiorz@gmail.com>
2020-06-22 19:03:35 -0500

    removed bounds

--- assets/js/mapbox.js
+++ assets/js/mapbox.js
@@ -1,16 +1,10 @@
 mapboxgl.accessToken = "pk.eyJ1IjoibHVjaWVubGl6bGVwaW9yeiIsImEiOiJjaWluY3lweWUwMWU5dHBrcHlsYnpscjF5In0.siT3_mzRABrCBeG4iGCEYQ";
 
-var bounds = [
-	[-88.045342, 41.612104], // Southwest coordinates
-	[-87.326560, 42.181465] // Northeast coordinates
-];
-
 var map = new mapboxgl.Map({
 	container: "map",
 	style: "mapbox://styles/mapbox/dark-v10",
 	center: [-87.695787, 41.881302], // Fred Hampton mural
 	zoom: 11,
-	maxBounds: bounds
 });
 
 var url = "assets/data/features.geojson";

d232e423cda46032170c566bacbea357997671f6
Lucien Liz-Lepiorz <lucien.lizlepiorz@gmail.com>
2020-06-23 22:00:34 -0500

    added AutoTable, PDF creation with data

--- assets/js/mapbox.js
+++ assets/js/mapbox.js
@@ -148,13 +148,68 @@ function highlightPoint(feature, address) {
 
 	// Add button listener
 	downloadButton.onclick = function(){
-		// Create PDF
-		var doc = new jsPDF();
-		doc.text(owner, 10, 10);
-		// Save with trimmed filename
-		doc.save(owner.replace(/\s+/g, "")+".pdf");
+		const currentOwner = feature.properties["Owner Name"];
+
+		const otherProperties = buildings.filter(function(feature) {
+			const otherPropertiesOwner = feature.properties["Owner Name"];
+			// Return feature when trimmed input is found in buildings array
+			return otherPropertiesOwner.indexOf(currentOwner) > -1;
+		});
+
+		console.log(JSON.stringify(otherProperties));
+
+
+		jsPDF.autoTableSetDefaults({
+			// Black table style
+    		headStyles: {fillColor: 0},
+  		});
+		
+		createPDF(owner, otherProperties);
 	};
 };
+
+function createPDF(title, list) {
+	// Create PDF
+	var doc = new jsPDF();
+
+	// Set column titles
+	var col = [
+		"Property Address",
+		"Community Area",
+		"Owner Name",
+		"Properties Held by Owner"
+	];
+	
+	// Create empty rows array
+	var rows = [];
+
+	// Add rows
+	list.forEach(feature => {
+		var temp = [
+			feature.properties["Property Address"],
+			feature.properties["Community Area"],
+			feature.properties["Owner Name"],
+			feature.properties["Properties Held by Owner"]
+		];
+		rows.push(temp);
+	});
+
+	// Create table
+	doc.autoTable({
+		columns: col,
+		body: rows,
+		didDrawPage: function (data) {
+  			// Set header
+  			doc.setFontSize(20)
+  			doc.setTextColor(40)
+  			doc.setFontStyle("bold")
+  			doc.text(title, data.settings.margin.left, 20)
+		},
+		margin: {top: 30},
+		});
+	// Save with trimmed filename
+	doc.save(title.replace(/\s+/g, "")+".pdf");
+}
 
 function clearPointStyles() {
 	for (var i = 0; i < buildings.length; i++) {

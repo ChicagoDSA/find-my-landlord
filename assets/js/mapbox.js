e2d91ded63c4d82e8af83dbe3fe16471ea25b201
Lucien Liz-Lepiorz <lucien.lizlepiorz@gmail.com>
2020-06-25 13:46:01 -0500

    optimize map style

--- assets/js/mapbox.js
+++ assets/js/mapbox.js
@@ -2,7 +2,7 @@
 mapboxgl.accessToken = "pk.eyJ1IjoibHVjaWVubGl6bGVwaW9yeiIsImEiOiJjaWluY3lweWUwMWU5dHBrcHlsYnpscjF5In0.siT3_mzRABrCBeG4iGCEYQ";
 var map = new mapboxgl.Map({
 	container: "map",
-	style: "mapbox://styles/mapbox/dark-v10",
+	style: "mapbox://styles/mapbox/dark-v10?optimize=true",
 	center: [-87.695787, 41.881302], // Fred Hampton mural
 	zoom: 10,
 });

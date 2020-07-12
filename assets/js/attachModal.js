a7f5adf83c540534598d62de553a6dd4e39521fb
Lucien Liz-Lepiorz <lucien.lizlepiorz@gmail.com>
2020-07-12 14:13:30 -0500

    removed static html

--- assets/js/attachModal.js
+++ assets/js/attachModal.js
@@ -1,22 +1,60 @@
 function attachModal(element) {
-	var modal = document.getElementById("data-info-modal");
-	var background = document.getElementById("modal-background");
-	var close = document.getElementById("modal-close");
+	// Create containers
+	var modal = document.createElement("div");
+	var background = document.createElement("div");
+	var content = document.createElement("div");
+	
+	// Title area
+	var titleContainer = document.createElement("div");
+	var titleText = document.createElement("h3");
+	var close = document.createElement("div");
+	var closeImg = document.createElement("img");
+	
+	// Content
+	var description = document.createElement("p");
+
+	// Set IDs
+	modal.id = "data-info-modal";
+	background.id = "modal-background";
+	content.id = "modal-content";
+	titleContainer.id = "modal-title";
+	close.id = "modal-close";
+
+	// Attributes
+	titleText.innerText= "How was this data collected?"
+	closeImg.src = "assets/images/times.svg";
+	description.innerText = "Content coming soon!";
+
+	// Append elements
+	modal.appendChild(background);
+	modal.appendChild(content);	
+	content.appendChild(titleContainer);
+	content.appendChild(close);
+	content.appendChild(description);
+	titleContainer.appendChild(titleText);
+	close.appendChild(closeImg);
 
 	// When element is clicked, show modal
 	element.onclick = function() {
-		modal.style.display = "block";
+		document.body.appendChild(modal);	
 	};
 
 	// Close button
 	close.onclick = function() {
-		modal.style.display = "none";
+		removeModal(modal);
 	};
 
 	// Background click
 	window.onclick = function(event) {
 		if (event.target == background) {
-			modal.style.display = "none";
+			removeModal(modal);
 		};
+	};
+};
+
+function removeModal (modal) {
+	if (modal) {
+		// Remove marker
+		modal.parentNode.removeChild(modal);
 	};
 };

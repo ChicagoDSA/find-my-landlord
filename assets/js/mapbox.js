c3cbfc1ee88866483d0e0430ade204db3731a24c
Lucien Liz-Lepiorz <lucien.lizlepiorz@gmail.com>
2020-07-12 23:02:56 -0500

    space

--- assets/js/mapbox.js
+++ assets/js/mapbox.js
@@ -1,6 +1,5 @@
 // Map setup
 mapboxgl.accessToken = "pk.eyJ1IjoibHVjaWVubGl6bGVwaW9yeiIsImEiOiJja2M2YTN3dG8wYmZlMnp0ZXBzZzJuM3JsIn0.n6bA8boNS3LQW1izwa6MKg";
-
 // Vars
 var defaultRadius = [
 	"interpolate",

832d8ad55149a1ff5ff7e9d4ce4a61bfb9bb87ac
Lucien Liz-Lepiorz <lucien.lizlepiorz@gmail.com>
2020-07-14 21:17:44 -0500

    added content and styles to modal

--- assets/js/attachModal.js
+++ assets/js/attachModal.js
@@ -24,7 +24,11 @@ function attachModal(element) {
 	// Attributes
 	titleText.innerText= "How was this data collected?"
 	closeImg.src = "assets/images/times.svg";
-	description.innerText = "Content coming soon!";
+	description.innerHTML = `
+		<h5>Note: properties may have changed ownership since the data was initially collected.</h5>
+		<p>The <b>property taxpayer</b> and address for buildings in Cook County were scraped from the <a href="https://www.cookcountyassessor.com/address-search">assessor website</a> in November 2019, and filtered to exclude properties not zoned for rental. Properties were matched based on the name and address of the property taxpayer, to find the <b>number of buildings</b> sharing the same taxpayer.</p>
+		<p>Matches were then researched and <b>affiliated</b> with particular corporations, managers, or landlords based on information in the <a href="https://www.ilsos.gov/corporatellc/CorporateLlcController">Illinois Secretary of State LLC Search</a> and other public records, including the <a href="https://cookrecorder.com/search-our-records/">Cook County Recorder of Deeds</a>.</p>
+	`;
 
 	// Append elements
 	modal.appendChild(background);

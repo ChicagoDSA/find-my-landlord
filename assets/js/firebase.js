b6e7eb754111cb5b6a4b960e04bc92e1c5f67fba
Lucien Liz-Lepiorz <lucien.lizlepiorz@gmail.com>
2020-07-21 13:20:00 -0500

    locked down DB

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -1,7 +1,22 @@
 // Initialize Cloud Firestore through Firebase
-firebase.initializeApp({
+var firebaseConfig = {
+	apiKey: "AIzaSyDfYrR3qErDogK_rqhzn49jlgshrJ6O_ic",
 	authDomain: "find-my-landlord.firebaseapp.com",
-	projectId: "find-my-landlord"
+	databaseURL: "https://find-my-landlord.firebaseio.com",
+	projectId: "find-my-landlord",
+	storageBucket: "find-my-landlord.appspot.com",
+	messagingSenderId: "470878094978",
+	appId: "1:470878094978:web:ebc5b2350e17f590af569b",
+	measurementId: "G-C7MQBH19E6"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+firebase.auth().signInAnonymously().catch(function(error) {
+	console.log(error.code);
+	console.log(error.message);
 });
 
 var db = firebase.firestore();

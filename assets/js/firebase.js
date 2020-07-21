b8d3fb76d51f087abf1e13a60f59763660049776
Lucien Liz-Lepiorz <lucien.lizlepiorz@gmail.com>
2020-07-20 19:12:32 -0500

    removed Firebase key

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -1,6 +1,5 @@
 // Initialize Cloud Firestore through Firebase
 firebase.initializeApp({
-	apiKey: "AIzaSyDpw9sXTjVLIMuUEjEGxGHFbZzldiWaXOQ",
 	authDomain: "find-my-landlord.firebaseapp.com",
 	projectId: "find-my-landlord"
 });

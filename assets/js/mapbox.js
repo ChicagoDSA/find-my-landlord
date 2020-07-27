f504b369854272930047612117ba15d14f8c06c9
Lucien Liz-Lepiorz <lucien.lizlepiorz@gmail.com>
2020-07-26 19:21:24 -0500

    disabling

--- assets/js/mapbox.js
+++ assets/js/mapbox.js
@@ -54,8 +54,7 @@ map.on("load", function() {
 		if (this.status >= 200 && this.status < 400) {
 			json = JSON.parse(this.response);
 
-			console.log([location.origin+location.pathname+"assets/data/features/{z}/{x}/{y}.pbf"]);
-
+/*
 			// Set source data
 			map.addSource("propertyData", {
 				type: "vector",
@@ -77,6 +76,7 @@ map.on("load", function() {
 			});
 
 			setHoverState("propertyData", "vector", "features");
+	*/
 
 			// Disable search if IE
 			if (checkIE() == true) {

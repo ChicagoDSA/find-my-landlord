9331c5d6c4dc7d0412045877c1255894b5ca7a91
Lucien Liz-Lepiorz <lucien.lizlepiorz@gmail.com>
2020-07-31 10:08:54 -0500

    fixed error handling for IE

--- assets/js/mapbox.js
+++ assets/js/mapbox.js
@@ -100,11 +100,11 @@ map.on("load", function() {
 	request.send();
 });
 
-map.on("error", e => {/*
+map.on("error", function(e) {
 	// Don't log empty tile errors
 	if (e && e.error.status != 403) {
 		console.error(e);
-	};*/
+	};
 });
 
 function addLayer (name, data, radius, color, opacity) {

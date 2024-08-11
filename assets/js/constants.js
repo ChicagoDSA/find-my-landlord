8132e4cc24190ac0740e3173aadb9c4b771df3c4
Nick Harris <nharris614@gmail.com>
2024-08-11 13:18:37 -0500

    feat: add static search index

--- assets/js/constants.js
+++ assets/js/constants.js
@@ -13,12 +13,14 @@ var relativeSizeColumn = "Relative Size";
 // Database reference
 var databaseCollectionName = "features-08-09-2020-05-48-PM";
 // JSON search
-var searchIndex = "https://find-my-landlord.nyc3.cdn.digitaloceanspaces.com/search-index-11-30-2020-06-30-PM.json";
+var searchIndex = "/assets/js/fml_searchindex_nov10.json";
 // Map tiles
-var tiles = "https://find-my-landlord.nyc3.cdn.digitaloceanspaces.com/features-08-09-2020-05-48-PM/{z}/{x}/{y}.pbf";
+var tiles =
+  "https://find-my-landlord.nyc3.cdn.digitaloceanspaces.com/features-08-09-2020-05-48-PM/{z}/{x}/{y}.pbf";
 
 // Mapbox key
-mapboxgl.accessToken = "pk.eyJ1IjoibHVjaWVubGl6bGVwaW9yeiIsImEiOiJja2M2YTN3dG8wYmZlMnp0ZXBzZzJuM3JsIn0.n6bA8boNS3LQW1izwa6MKg";
+mapboxgl.accessToken =
+  "pk.eyJ1IjoibHVjaWVubGl6bGVwaW9yeiIsImEiOiJja2M2YTN3dG8wYmZlMnp0ZXBzZzJuM3JsIn0.n6bA8boNS3LQW1izwa6MKg";
 
 // Colors
 var dsaRed = "#ec1f27";
@@ -32,45 +34,49 @@ var white = "#fff";
 var gray = "#808080";
 
 // Map defaults
-var defaultOpacity = .5;
+var defaultOpacity = 0.5;
 var highlightZoom = 14;
 
 // Change colors based on landlord size
 var defaultColors = [
-	"case",
-	[">=", ["get", ownedColumn], 100],
-	color4,
-	[">=", ["get", ownedColumn], 10],
-	color3,
-	[">=", ["get", ownedColumn], 3],
-	color2,
-	[">", ["get", ownedColumn], 0],
-	color1,
-	white
+  "case",
+  [">=", ["get", ownedColumn], 100],
+  color4,
+  [">=", ["get", ownedColumn], 10],
+  color3,
+  [">=", ["get", ownedColumn], 3],
+  color2,
+  [">", ["get", ownedColumn], 0],
+  color1,
+  white,
 ];
 
 // Scale radius based on zoom, relative unit size, hover
 var defaultRadius = [
-	"interpolate",
-	["exponential", 1.75],
-	["zoom"],
-	8, 
-	["case",
-		["boolean", ["feature-state", "hover"], false],
-		["interpolate", ["linear"], ["get", relativeSizeColumn], 0, 10, 100, 20],
-		["interpolate", ["linear"], ["get", relativeSizeColumn], 0, 2, 100, 10]
-	],
-	16, 
-	["case",
-		["boolean", ["feature-state", "hover"], false],
-		["interpolate", ["linear"], ["get", relativeSizeColumn], 0, 12, 100, 24],
-		["interpolate", ["linear"], ["get", relativeSizeColumn], 0, 4, 100, 20]
-	],
-	22, ["case",
-		["boolean", ["feature-state", "hover"], false],
-		["interpolate", ["linear"], ["get", relativeSizeColumn], 0, 200, 100, 400],
-		["interpolate", ["linear"], ["get", relativeSizeColumn], 0, 180, 100, 900]
-	]
+  "interpolate",
+  ["exponential", 1.75],
+  ["zoom"],
+  8,
+  [
+    "case",
+    ["boolean", ["feature-state", "hover"], false],
+    ["interpolate", ["linear"], ["get", relativeSizeColumn], 0, 10, 100, 20],
+    ["interpolate", ["linear"], ["get", relativeSizeColumn], 0, 2, 100, 10],
+  ],
+  16,
+  [
+    "case",
+    ["boolean", ["feature-state", "hover"], false],
+    ["interpolate", ["linear"], ["get", relativeSizeColumn], 0, 12, 100, 24],
+    ["interpolate", ["linear"], ["get", relativeSizeColumn], 0, 4, 100, 20],
+  ],
+  22,
+  [
+    "case",
+    ["boolean", ["feature-state", "hover"], false],
+    ["interpolate", ["linear"], ["get", relativeSizeColumn], 0, 200, 100, 400],
+    ["interpolate", ["linear"], ["get", relativeSizeColumn], 0, 180, 100, 900],
+  ],
 ];
 
 // Custom UI
@@ -81,7 +87,9 @@ var spinner = document.getElementById("spinner");
 var searchInputContainer = document.getElementById("search-input-container");
 var searchInput = document.getElementById("search-input");
 var clearButton = document.getElementById("clear");
-var searchResultsContainer = document.getElementById("search-results-container");
+var searchResultsContainer = document.getElementById(
+  "search-results-container"
+);
 var searchResultsCounter = document.getElementById("search-results-counter");
 var searchResultsList = document.getElementById("search-results-list");
 var selectedContainer = document.getElementById("selected-container");
